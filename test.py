fb318ed52f0965e120dc347aff182f42905f7531
Slightly <2392568162@qq.com>
2020-05-25 22:17:59 +0800

    change

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ def add_num(a, b):
     return n
 
 if __name__ == "__main__":
+    m0 = int(input())
     m1 = int(input())
-    m2 = int(input())
-    s = add_num(m1, m2)
+    s = add_num(m0, m1)
     print(s)
 
